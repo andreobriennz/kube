13fc45c1e120f4141d06f044843785279aa6ff01
andreobriennz <andre@communica.co.nz>
2018-06-21 16:36:54 +1200

    Update package.json and gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,10 @@
+
 const gulp = require('gulp');
 
 const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const babel = require('gulp-babel');
 
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
@@ -10,16 +13,55 @@ const minify = require('gulp-clean-css');
 const settings = {
     dist: 'dist', 
     styles: [ 
-        'src/normalize.scss',
-        'src/skeleton.scss',
+        // kube:
+        'src/kube/src/_scss/_variables.scss',
+        'src/kube/src/_scss/mixins/_breakpoints.scss',
+        'src/kube/src/_scss/mixins/_fonts.scss',
+        'src/kube/src/_scss/mixins/_flex.scss',
+        // 'src/kube/src/_scss/mixins/_grid.scss',
+        'src/kube/src/_scss/mixins/_utils.scss',
+        'src/kube/src/_scss/mixins/_buttons.scss',
+        'src/kube/src/_scss/mixins/_gradients.scss',
+        'src/kube/src/_scss/mixins/_labels.scss',
+
+        // flexbox-grid:
         'src/flexbox-grid.scss',
     ],
+    scripts: [
+        // kube:
+        'src/_js/Core/Kube.js',
+        'src/_js/Core/Kube.Plugin.js',
+        'src/_js/Core/Kube.Animation.js',
+        'src/_js/Core/Kube.Detect.js',
+        'src/_js/Core/Kube.FormData.js',
+        'src/_js/Core/Kube.Response.js',
+        'src/_js/Core/Kube.Utils.js',
+        'src/_js/Message/Kube.Message.js',
+        'src/_js/Sticky/Kube.Sticky.js',
+        'src/_js/Toggleme/Kube.Toggleme.js',
+        'src/_js/Offcanvas/Kube.Offcanvas.js',
+        'src/_js/Collapse/Kube.Collapse.js',
+        'src/_js/Dropdown/Kube.Dropdown.js',
+        'src/_js/Tabs/Kube.Tabs.js',
+        'src/_js/Modal/Kube.Modal.js',
+    ],
 }
+
+// JAVASCRIPT: concat together, compile es2015, minify
+gulp.task('scripts', function() {
+    return gulp.src( settings.scripts ) 
+        .pipe(concat( 'app.js' )) 
+        .pipe(babel()) 
+        .pipe(gulp.dest( settings.dist )) 
+        .pipe(rename( 'app.min.js' ))
+        .pipe(uglify())
+        .pipe(gulp.dest( settings.dist ));
+});
 
 // CSS/SASS: compile scss to css, autoprefix, minify
 gulp.task('sass', function () {
     return gulp.src( settings.styles ) 
-        .pipe(concat( 'skeleton-flexbox.scss' ))
+        .pipe(concat( 'kube-flexbox.scss' ))
         .pipe(gulp.dest( settings.dist ))
 
         .pipe(sass().on('error', sass.logError))
@@ -27,16 +69,17 @@ gulp.task('sass', function () {
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(rename('skeleton-flexbox.css')) 
+        .pipe(rename('kube-flexbox.css')) 
         .pipe(gulp.dest( settings.dist )) 
-        .pipe(rename('skeleton-flexbox.min.css'))
+        .pipe(rename('kube-flexbox.min.css'))
         .pipe(minify({compatibility: 'ie8'})) 
         .pipe(gulp.dest( settings.dist ));
 });
 
 // WATCH FOR CHANGES
 gulp.task('watch', function() {
+    gulp.watch( settings.scripts, ['scripts']);
     gulp.watch( settings.styles, ['sass']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'scripts', 'watch']);
